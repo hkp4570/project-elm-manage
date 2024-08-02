ac08091ebf40c1f7884c9001c21c579d134f09b5
hkp <colourful4570@163.com>
2024-08-02 09:52:36 +0800

    项目初始化

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports={
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    "indent": ["error", "tab"]
+  }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# project-elm-manage
-project-elm的后台管理系统
